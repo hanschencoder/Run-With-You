6e54894de3a83fa351c988825df0a7ea70800ac3
shensky711 <shensky711@gmail.com>
2016-11-10 17:30:07 +0800

    add bottombar and toolbar

--- runner/src/main/java/com/hanschen/runwithyou/MainActivity.java
+++ runner/src/main/java/com/hanschen/runwithyou/MainActivity.java
@@ -1,15 +1,47 @@
 package com.hanschen.runwithyou;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 import site.hanschen.common.base.activity.BaseActivity;
 
+import static com.hanschen.runwithyou.R.id.bottomBar;
+
 
 public class MainActivity extends BaseActivity {
+
+    private Toolbar   mToolbar;
+    private BottomBar mBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+        setSupportActionBar(mToolbar);
+
+        mBottomBar = (BottomBar) findViewById(bottomBar);
+        mBottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                // TODO: 2016/11/10     
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
--- runner/src/main/java/com/hanschen/runwithyou/application/RunnerApplication.java
+++ runner/src/main/java/com/hanschen/runwithyou/application/RunnerApplication.java
@@ -1,12 +1,21 @@
 package com.hanschen.runwithyou.application;
 
 
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
 import site.hanschen.common.base.application.BaseApplication;
 
 /**
  * @author Hans.Chen
  */
 public class RunnerApplication extends BaseApplication {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
 
     @Override
     public void onCreate() {
