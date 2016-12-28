5a934d0c95d42284f4bfdbd921e13f96299d79e9
shensky711 <shensky711@gmail.com>
2016-12-28 16:19:43 +0800

    setting for foreground service and request white list

--- runner/src/main/java/site/hanschen/runwithyou/main/me/MeFragment.java
+++ runner/src/main/java/site/hanschen/runwithyou/main/me/MeFragment.java
@@ -1,11 +1,16 @@
 package site.hanschen.runwithyou.main.me;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.widget.Toast;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import site.hanschen.runwithyou.R;
 
@@ -19,26 +24,55 @@ public class MeFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.me_preferences);
-        setPrefListeners();
+        initPreferences();
     }
 
-    private void setPrefListeners() {
+    private void initPreferences() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            ((PreferenceGroup) findPreference(getString(R.string.pref_category_memory_resident))).removePreference(findPreference(
+                    getString(R.string.pref_memory_resident_white_list)));
+        }
+    }
 
-        Preference updatePref = findPreference("pref_etc_check_update");
-        updatePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            public boolean onPreferenceClick(Preference preference) {
-                Toast.makeText(getActivity().getApplicationContext(), "TODO:", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        handlePreferenceClick(preference);
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
 
-        Preference issuePref = findPreference("pref_etc_issue");
-        issuePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            public boolean onPreferenceClick(Preference preference) {
+    private void handlePreferenceClick(Preference preference) {
+        if (preference == null || preference.getKey() == null) {
+            return;
+        }
+        switch (preference.getKey()) {
+            case "pref_memory_resident_white_list":
+                requestPowerWhiteList();
+                break;
+            case "pref_etc_check_update":
+                // TODO: 2016/12/28
+                break;
+            case "pref_etc_issue":
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/shensky711/Run-With-You/issues"));
                 startActivity(intent);
-                return true;
-            }
-        });
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void requestPowerWhiteList() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return;
+        }
+        String packageName = getActivity().getPackageName();
+        PowerManager pm = (PowerManager) getActivity().getSystemService(Context.POWER_SERVICE);
+        Intent intent = new Intent();
+        if (pm.isIgnoringBatteryOptimizations(packageName)) {
+            intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+        } else {
+            intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:" + packageName));
+        }
+        startActivity(intent);
     }
 }
--- runner/src/main/java/site/hanschen/runwithyou/service/RunnerService.java
+++ runner/src/main/java/site/hanschen/runwithyou/service/RunnerService.java
@@ -1,19 +1,31 @@
 package site.hanschen.runwithyou.service;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.RemoteCallbackList;
 import android.os.RemoteException;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.Locale;
+
+import site.hanschen.runwithyou.R;
+import site.hanschen.runwithyou.main.MainActivity;
 
 /**
  * @author HansChen
  */
 public class RunnerService extends Service {
+
+    private static final int NOTIFICATION_ID = 1;
 
     public static void bind(Context context, ServiceConnection conn) {
         Intent intent = new Intent(context, RunnerService.class);
@@ -28,16 +40,21 @@ public class RunnerService extends Service {
     private final    Handler                            mMainHandler = new Handler(Looper.getMainLooper());
     private final    RemoteCallbackList<RunnerCallback> mCallbacks   = new RemoteCallbackList<>();
     private volatile int                                mStepCount   = 65;
+    private SharedPreferences mPreferences;
 
     @Override
     public void onCreate() {
         super.onCreate();
         mContext = this;
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mPreferences.registerOnSharedPreferenceChangeListener(mOnPreferenceChangeListener);
+        setForegroundState(mPreferences);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mPreferences.unregisterOnSharedPreferenceChangeListener(mOnPreferenceChangeListener);
     }
 
     @Override
@@ -48,6 +65,45 @@ public class RunnerService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return new RunnerManagerImpl();
+    }
+
+    private SharedPreferences.OnSharedPreferenceChangeListener mOnPreferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            switch (key) {
+                case "pref_memory_resident_foreground_service":
+                    setForegroundState(sharedPreferences);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    private void setForegroundState(SharedPreferences sharedPreferences) {
+        if (sharedPreferences.getBoolean("pref_memory_resident_foreground_service", true)) {
+            startForeground();
+        } else {
+            stopForeground();
+        }
+    }
+
+    private void startForeground() {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher)
+                                                                        .setContentTitle(getString(R.string.app_name))
+                                                                        .setContentText(String.format(Locale.getDefault(),
+                                                                                                      "当日步数: %d",
+                                                                                                      mStepCount))
+                                                                        .setContentIntent(pendingIntent)
+                                                                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                                                                        .build();
+        startForeground(NOTIFICATION_ID, notification);
+    }
+
+    private void stopForeground() {
+        stopForeground(true);
     }
 
     public final class RunnerManagerImpl extends RunnerManager.Stub {
